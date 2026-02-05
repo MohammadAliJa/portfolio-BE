2c6177fbcf0108ec66af65f88c353f751e73c9c8
Mohammad Ali Jarjoumah <125255089+MohammadAliJa@users.noreply.github.com>
2026-02-05 11:49:14 +0200

    Hiding .env files

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 require('dotenv').config({
-    path: path.join(__dirname, '../.env')
+    path: path.join(__dirname, '../environments/.env')
 });
 const { connectMongoDB } = require('./config/db-mongo');
 
